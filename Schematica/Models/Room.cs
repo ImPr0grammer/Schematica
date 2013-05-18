90784409af3d240fb5a759c69f0be24814ab69d1
gtarasov <gleb34@gmail.com>
2013-05-18 21:20:13 +0400

    model

---
+++ Schematica/Models/Room.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using Newtonsoft.Json;
+
+namespace SchemeEditor.Schema
+{
+    class Room : IJsonObject
+    {
+        public string Title { get; set; }
+        public string Id { get; set; }
+        public Color Color { get; set; }
+        public Point[] Points { get; set; }
+
+        public string GetHexColor()
+        {
+            return "#" + Color.R.ToString("X2") + Color.G.ToString("X2") + Color.B.ToString("X2");
+        }
+
+        public Dictionary<string, object> ToJsonObject()
+        {
+            return new Dictionary<string, object>
+            {
+                { "title", Title },
+                { "id", Id },
+                { "color", GetHexColor() },
+                { "points", Points.Select(p => new[] { p.X, p.Y }) }
+            };
+        }
+    }
+}
