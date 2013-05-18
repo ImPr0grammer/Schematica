073f315037fe232e3ae0574dda559e5249751ebc
Aleksei <alekseev.aleksei@gmail.com>
2013-05-18 14:05:21 +0400

    WallTool

---
+++ Schematica/Scripts/Editor/WallTool.js
@@ -0,0 +1,27 @@
+﻿function WallTool(ctx) {
+
+    function onclick(event) {
+
+    }
+
+    function mouseDown(event) {
+        
+    }
+    
+    function mouseUp(event) {
+        
+    }
+    
+    function move(event) {
+        
+    }
+    
+    function deActivate() {
+        
+    }
+    
+    function activate() {
+        
+    }
+
+}
