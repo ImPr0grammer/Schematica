29e062bfea50ab360c26ff265e6692091edd6d7e
Aleksei <alekseev.aleksei@gmail.com>
2013-05-18 12:47:57 +0400

    Schematica
    Залил проект

---
+++ Schematica/Controllers/SchemeEditorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Schematica.Controllers
+{
+    public class SchemeEditorController : Controller
+    {
+        //
+        // GET: /SchemeEditor/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Schematica/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Schematica.MvcApplication" Language="C#" %>
